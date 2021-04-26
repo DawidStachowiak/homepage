a111a019d8d07874552abea8b09df52bc1f93cb2
Dawid Stachowiak <stasiu-6@wp.pl>
2021-04-26 16:21:27 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+console.log ("Cześć wszystkim");
+
+let button = document.querySelector(".js-image__remove");
+let imageRemove = document.querySelector(".image");
+let buttonMenu = document.querySelector(".js-menu__remove");
+let navigation = document.querySelector(".navigation");
+
+
+
+button.addEventListener("click", () => {
+  imageRemove.classList.toggle("hidden__image");
+  button.innerText = imageRemove.classList.contains("hidden__image") ? "Pokaż zdjęcie" : "Ukryj zdjęcie";
+
+});
+ 
+
+buttonMenu.addEventListener("click", () =>{
+navigation.classList.toggle("hide__navigation");
+buttonMenu.innerText = navigation.classList.contains("hide__navigation") ? "Pokaż menu" : "Ukryj menu";
+}
+);
+
+
+
