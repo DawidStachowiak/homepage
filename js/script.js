66d5d2ce0dd9dcf651a5f50d1d134c386f198493
Dawid Stachowiak <dawid@zetit.pl>
2021-04-28 22:32:31 +0200

    change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 {
   const welcome = () => {
     console.log("Cześć wszystkim!");
-  };
+  }
 
   const oneClickImageRemove = () => {
-    let button = document.querySelector(".js-image__remove");
-    let imageRemove = document.querySelector(".image");
+    const button = document.querySelector(".js-image__remove");
+    const imageRemove = document.querySelector(".image");
     imageRemove.classList.toggle("hidden__image");
     button.innerText = imageRemove.classList.contains("hidden__image")
       ? "Pokaż zdjęcie"
@@ -13,23 +13,26 @@
   };
 
   const oneClickMenuToggle = () => {
-    const buttonMenu = document.querySelector(".js-menu__remove");
+    
     const navigation = document.querySelector(".navigation");
 
     navigation.classList.toggle("hide__navigation");
-
     buttonMenu.innerText = navigation.classList.contains("hide__navigation")
       ? "Pokaż menu"
       : "Ukryj menu";
   };
 
   const init = () => {
+    const button = document.querySelector(".js-image__remove");
+    button.addEventListener("click", oneClickImageRemove);
     const buttonMenu = document.querySelector(".js-menu__remove");
     buttonMenu.addEventListener("click", oneClickMenuToggle);
-    button.addEventListener("click", oneClickImageRemove);
-  };
+    
 
-  welcome();
+    welcome();
+  };
+  
+  
 
   init();
 }
